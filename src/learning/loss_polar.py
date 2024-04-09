Fix loss calculation

src/learning/loss_polar.py
@@ -8,7 +8,8 @@ class WeightedBceLoss(nn.Module):
         super(WeightedBceLoss, self).__init__()
         self.w = w
 
-    def forward(self, y_true, y_pred):
+    def forward(self, y_true_double, y_pred_double):
+        y_true, y_pred = y_true_double.float(), y_pred_double.float()
         bce = F.binary_cross_entropy(y_pred, y_true, reduction='none')
         certainty_weights = ((torch.abs(y_pred - 0.5) * 2) ** (-self.w)).detach()  # ensure no gradients for weights
         weighted_loss = bce * certainty_weights

src/learning/train.py
@@ -20,7 +20,7 @@ from evaluate import test_model
 def get_device():
     if torch.cuda.is_available():
         print('GPU is available.')
-        device = torch.device("cuda")
+        device = torch.device("cuda:1")
     else:
         print('GPU is not available, using CPU.')
         device = torch.device("cpu")
@@ -69,7 +69,7 @@ def train(
 
     best_valid_loss = float('inf')
     os.makedirs(model_folder, exist_ok=True)
-    model_path = os.path.join(model_folder, f'model_march24_1C')
+    model_path = os.path.join(model_folder, f'model_april2_1C')
 
     device = get_device()
     if use_polar: